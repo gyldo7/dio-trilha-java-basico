8729cef11050318d6ae97f2fc76dbb0faddf0750
José Gildo <gyldo7@gmail.com>
2025-03-14 22:25:07 -0300

    Aula Operadores-4 concluída.

---
+++ tipos-variaveis/src/Operadores4.java
@@ -0,0 +1,33 @@
+public class Operadores4 {
+    public static void main(String[] args) {
+        
+        int numero = 5;
+        //x repetição
+        numero = numero + 1; // 6
+        numero++; // 7
+
+        System.out.println(numero); // 7
+
+        System.out.println(numero++); // 7
+
+        System.out.println(numero); // 8
+
+        System.out.println(++numero); // 9
+
+        System.out.println(numero--);
+
+        System.out.println(numero);
+
+        
+        boolean variavel = true;
+        
+        System.out.println(!variavel);
+
+        System.out.println(variavel);
+
+        variavel = !variavel;
+
+        System.out.println(variavel);
+
+    }
+} 
